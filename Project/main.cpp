ba9d628d360daaab2a05e2fd925a0338175c50a5
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-09-30 12:53:30 +0900

    - 再帰関数の実装

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 
-template <typename T>
-inline T Min(T _x, T _y)
+inline unsigned int CalcurateRecursiveSalary(unsigned int _hourWork)
 {
-    // 小さい方を返す
-    return _x < _y ? _x : _y;
-}
+    // 働け (早期リターン)
+    if (_hourWork <= 0u) return 0u;
 
-template <>
-inline char Min(char, char)
-{
-    printf("数字以外は代入できません\n");
-    return {};
+    // 末端
+    if (_hourWork <= 1u)
+    {
+        return 100u;
+    }
+
+    // 再起的に実行
+    return CalcurateRecursiveSalary(_hourWork - 1) * 2u - 50u;
 }
 
 int main(void)
 {
-    /// int型
-    int iNum0 = 16; int iNum1 = -4; int iResult = {};
-    iResult = Min(iNum0, iNum1);
-    printf("Min(%d, %d) : %d\n", iNum0, iNum1, iResult);
+    unsigned int hourlyWage = 1072u;
 
-    /// float型
-    float fNum0 = 32.0f; float fNum1 = 64.0f; float fResult = {};
-    fResult = Min(fNum0, fNum1);
-    printf("Min(%.1f, %.1f) : %.1f\n", fNum0, fNum1, fResult);
-    
-    /// double型
-    double lfNum0 = 4.0; double lfNum1 = -4.0; double lfResult = {};
-    lfResult = Min(lfNum0, lfNum1);
-    printf("Min(%.1lf, %.1f) : %.1lf\n", lfNum0, lfNum1, lfResult);
+    /// 一般的な給料計算方式
+    printf("Normal Salary-Calculate-Type :\n");
+    for (unsigned int hourWork = 1u; hourWork <= 8u; ++hourWork)
+    {
+        
+        printf("%2u hour : %5u yen\n", hourWork, hourWork * hourlyWage);
+    }
 
-    /// char型
-    char c0 = 65u; char c1 = 66u; char cResult = {};
-    cResult = Min(c0, c1);
-    printf("Min(%c, %c) : %c\n", c0, c1, cResult);
+    printf("\n");
 
+    printf("Recursive Salary-Calcurate-Type :\n");
+    for (unsigned int hourWork = 1u; hourWork <= 8u; ++hourWork)
+    {
+        printf("%2u hour : %5u yen\n", hourWork, CalcurateRecursiveSalary(hourWork));
+    }
 
     return 0;
 }
