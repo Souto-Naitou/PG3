6620a975532af926e3630f0141d229d0f350dc6c
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-09-30 14:04:53 +0900

    - answer.mdの追加

--- Project/answer.md
+++ Project/answer.md
@@ -1,19 +1,27 @@
-Normal Salary-Calculate-Type :
- 1 hour :  1072 yen
- 2 hour :  2144 yen
- 3 hour :  3216 yen
- 4 hour :  4288 yen
- 5 hour :  5360 yen
- 6 hour :  6432 yen
- 7 hour :  7504 yen
- 8 hour :  8576 yen
+## Normal Salary-Calculate-Type
+WorkTime | Salary
+---|---:
+ 1 hour |  1072 yen  
+ 2 hour |  2144 yen  
+ 3 hour |  3216 yen  
+ 4 hour |  4288 yen  
+ 5 hour |  5360 yen  
+ 6 hour |  6432 yen  
+ 7 hour |  7504 yen  
+ 8 hour |  8576 yen  
+ 9 hour |  9648 yen  
+10 hour | 10720 yen  
 
-Recursive Salary-Calcurate-Type :
- 1 hour :   100 yen
- 2 hour :   150 yen
- 3 hour :   250 yen
- 4 hour :   450 yen
- 5 hour :   850 yen
- 6 hour :  1650 yen
- 7 hour :  3250 yen
- 8 hour :  6450 yen
+## Recursive Salary-Calculate-Type
+WorkTime | Salary
+---|---:
+ 1 hour |   100 yen  
+ 2 hour |   150 yen  
+ 3 hour |   250 yen  
+ 4 hour |   450 yen  
+ 5 hour |   850 yen  
+ 6 hour |  1650 yen  
+ 7 hour |  3250 yen  
+ 8 hour |  6450 yen  
+ 9 hour | 12850 yen  
+10 hour | 25650 yen  
--- Project/main.cpp
+++ Project/main.cpp
@@ -2,7 +2,7 @@
 
 inline unsigned int CalculateRecursiveSalary(unsigned int _hourWork)
 {
-    // 働け (早期リターン)
+    // 早期[リターン|退社]
     if (_hourWork <= 0u) return 0u;
 
     // 末端
@@ -18,21 +18,25 @@ inline unsigned int CalculateRecursiveSalary(unsigned int _hourWork)
 int main(void)
 {
     unsigned int hourlyWage = 1072u;
+    unsigned int hourWork = 0u;
+
+    printf("実働時間を入力 > ");
+    scanf_s("%u", &hourWork);
 
     /// 一般的な賃金体系
     printf("Normal Salary-Calculate-Type :\n");
-    for (unsigned int hourWork = 1u; hourWork <= 8u; ++hourWork)
+    for (unsigned int i = 1u; i <= hourWork; ++i)
     {
-        printf("%2u hour : %5u yen\n", hourWork, hourWork * hourlyWage);
+        printf("%2u hour : %5u yen\n", i, i * hourlyWage);
     }
 
     printf("\n");
 
     /// 再帰的な賃金体系
     printf("Recursive Salary-Calculate-Type :\n");
-    for (unsigned int hourWork = 1u; hourWork <= 8u; ++hourWork)
+    for (unsigned int i = 1u; i <= hourWork; ++i)
     {
-        printf("%2u hour : %5u yen\n", hourWork, CalculateRecursiveSalary(hourWork));
+        printf("%2u hour : %5u yen\n", i, CalculateRecursiveSalary(i));
     }
 
     return 0;
