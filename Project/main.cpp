e50e67986092dc6b4563354f0c274490a683c7ac
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-10-07 11:55:00 +0900

    完成

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,43 +1,65 @@
-#include <iostream>
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <Windows.h>
 
-inline unsigned int CalculateRecursiveSalary(unsigned int _hourWork)
-{
-    // 早期[リターン|退社]
-    if (_hourWork <= 0u) return 0u;
+typedef void (*PFunc)();
 
-    // 末端
-    if (_hourWork <= 1u)
-    {
-        return 100u;
-    }
+int isOdd = {};
+int diceResult = {};
 
-    // 再起的に実行
-    return CalculateRecursiveSalary(_hourWork - 1) * 2u - 50u;
-}
+void SetTimeout(PFunc _pre, PFunc _later, int _sec);
+void InputPrediction();
+void Roll();
+void DisplayResult();
+void DisplayAnswer();
 
 int main(void)
 {
-    unsigned int hourlyWage = 1072u;
-    unsigned int hourWork = 0u;
+    srand(static_cast<unsigned int>(time(nullptr)));
 
-    printf("実働時間を入力 > ");
-    scanf_s("%u", &hourWork);
-
-    /// 一般的な賃金体系
-    printf("Normal Salary-Calculate-Type :\n");
-    for (unsigned int i = 1u; i <= hourWork; ++i)
-    {
-        printf("%2u hour : %5u yen\n", i, i * hourlyWage);
-    }
-
-    printf("\n");
-
-    /// 再帰的な賃金体系
-    printf("Recursive Salary-Calculate-Type :\n");
-    for (unsigned int i = 1u; i <= hourWork; ++i)
-    {
-        printf("%2u hour : %5u yen\n", i, CalculateRecursiveSalary(i));
-    }
+    SetTimeout(InputPrediction, Roll, 3);
 
     return 0;
+}
+
+void SetTimeout(PFunc _pre, PFunc _later, int _sec)
+{
+    _pre();
+    Sleep(_sec * 1000);
+    _later();
+}
+
+void InputPrediction()
+{
+    printf("出目が偶数の場合 0 を入力\n");
+    printf("出目が奇数の場合 1 を入力\n");
+    printf("> ");
+    scanf_s("%d", &isOdd);
+    printf("\n");
+    printf("予想は%s！ 正解は...\n", isOdd ? "奇数" : "偶数");
+}
+
+void Roll()
+{
+    diceResult = rand() % 6 + 1;
+    SetTimeout(DisplayResult, DisplayAnswer, 1);
+}
+
+void DisplayResult()
+{
+    printf("サイコロの出目は「%d」\n", diceResult);
+}
+
+void DisplayAnswer()
+{
+    if (diceResult % 2 == isOdd)
+    {
+        printf("正解\n");
+    }
+    else
+    {
+        printf("不正解\n");
+    }
 }
