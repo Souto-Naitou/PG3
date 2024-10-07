a24143fc095a5c975265b6e2c1b5a2453801afde
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-10-07 12:30:24 +0900

    完了

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,65 +1,69 @@
 ﻿#include <iostream>
 #include <stdlib.h>
 #include <time.h>
-#include <string>
 #include <Windows.h>
+#include <functional>
 
-typedef void (*PFunc)();
-
-int isOdd = {};
-int diceResult = {};
-
-void SetTimeout(PFunc _pre, PFunc _later, int _sec);
-void InputPrediction();
-void Roll();
-void DisplayResult();
-void DisplayAnswer();
+void SetTimeout(std::function<void()> _pre, std::function<void()> _later, int _sec);
 
 int main(void)
 {
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    SetTimeout(InputPrediction, Roll, 3);
+    /// 変数
+    int isOdd = {};
+    int diceResult = {};
+
+    std::function<void()> fxpInputPrediction;
+    std::function<void()> fxpRoll;
+    std::function<void()> fxpDisplayResult;
+    std::function<void()> fxpDisplayAnswer;
+
+    fxpInputPrediction = [&](void)
+    {
+        printf("出目が偶数の場合 0 を入力\n");
+        printf("出目が奇数の場合 1 を入力\n");
+        printf("> ");
+        scanf_s("%d", &isOdd);
+        printf("\n");
+        printf("予想は%s！ 正解は...\n", isOdd ? "奇数" : "偶数");
+        return;
+    };
+
+    fxpRoll = [&](void)
+    {
+        diceResult = rand() % 6 + 1;
+        SetTimeout(fxpDisplayResult, fxpDisplayAnswer, 1);
+    };
+
+    fxpDisplayResult = [&](void)
+    {
+        printf("サイコロの出目は「%d」\n", diceResult);
+        return;
+    };
+
+    fxpDisplayAnswer = [&](void)
+    {
+        if (diceResult % 2 == isOdd)
+        {
+            printf("正解\n");
+        }
+        else
+        {
+            printf("不正解\n");
+        }
+        return;
+    };
+
+    // 呼び出し
+    SetTimeout(fxpInputPrediction, fxpRoll, 3);
 
     return 0;
 }
 
-void SetTimeout(PFunc _pre, PFunc _later, int _sec)
+void SetTimeout(std::function<void()> _pre, std::function<void()> _later, int _sec)
 {
     _pre();
     Sleep(_sec * 1000);
     _later();
-}
-
-void InputPrediction()
-{
-    printf("出目が偶数の場合 0 を入力\n");
-    printf("出目が奇数の場合 1 を入力\n");
-    printf("> ");
-    scanf_s("%d", &isOdd);
-    printf("\n");
-    printf("予想は%s！ 正解は...\n", isOdd ? "奇数" : "偶数");
-}
-
-void Roll()
-{
-    diceResult = rand() % 6 + 1;
-    SetTimeout(DisplayResult, DisplayAnswer, 1);
-}
-
-void DisplayResult()
-{
-    printf("サイコロの出目は「%d」\n", diceResult);
-}
-
-void DisplayAnswer()
-{
-    if (diceResult % 2 == isOdd)
-    {
-        printf("正解\n");
-    }
-    else
-    {
-        printf("不正解\n");
-    }
 }
