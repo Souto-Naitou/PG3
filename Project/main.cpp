f2df892dca08ff5da3a77eb1deb3306382176148
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-11-13 06:38:51 +0900

    Enemy クラスの削除と Combination クラスの追加
    `Enemy.cpp` と `Enemy.h` を削除し、`Combination.h` を追加しました。
    `Project.vcxproj` と `Project.vcxproj.filters` から `Enemy` の参照を削除し、`Combination` を追加しました。
    `main.cpp` で `Enemy` のインクルードと使用を `Combination` に置き換えました。
    `.editorconfig` ファイルを追加し、インデントスタイルや文字セットを設定しました。

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,21 +1,42 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdlib.h>
 #include <time.h>
 #include <Windows.h>
 #include <functional>
 
-#include "Enemy.h"
+#include "Combination.h"
 
 int main(void)
 {
-    Enemy* enemy = new Enemy();
+    /// それぞれの型で2つの変数を作成
+    int     iNum0 = 32;
+    int     iNum1 = 64;
 
-    for (int i = 0; i < 10; i++)
-    {
-        enemy->Update();
-        enemy->ChangeState();
-        Sleep(1000);
-    }
+    float   fNum0 = -64.0f;
+    float   fNum1 = 128.0f;
+
+    double  dNum0 = -128.0;
+    double  dNum1 = -16.0;
+
+
+    /// 各変数を組み合わせたインスタンスを作成
+    Combination<int,    int>*       iCombi  = new Combination(iNum0, iNum1);
+    Combination<float,  float>*     fCombi  = new Combination(fNum0, fNum1);
+    Combination<double, double>*    dCombi  = new Combination(dNum0, dNum1);
+
+    Combination<int,    float>*     ifCombi = new Combination(iNum0, fNum0);
+    Combination<float,  double>*    fdCombi = new Combination(fNum0, dNum0);
+    Combination<int,    double>*    idCombi = new Combination(iNum0, dNum0);
+
+
+    /// 全インスタンスの最小値を表示
+    std::cout << "[" << iCombi->x << ":" << iCombi->y << "]->Min() : " << iCombi->Min() << std::endl;
+    std::cout << "[" << fCombi->x << ":" << fCombi->y << "]->Min() : " << fCombi->Min() << std::endl;
+    std::cout << "[" << dCombi->x << ":" << dCombi->y << "]->Min() : " << dCombi->Min() << std::endl;
+    std::cout << "[" << ifCombi->x << ":" << ifCombi->y << "]->Min() : " << ifCombi->Min() << std::endl;
+    std::cout << "[" << fdCombi->x << ":" << fdCombi->y << "]->Min() : " << fdCombi->Min() << std::endl;
+    std::cout << "[" << idCombi->x << ":" << idCombi->y << "]->Min() : " << idCombi->Min() << std::endl;
+
 
     return 0;
 }
