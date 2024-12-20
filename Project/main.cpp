78ac5f2d8481bb2b73062cf2731873804df08ffa
Souto-Naitou <k023g0031@g.neec.ac.jp>
2024-12-20 16:03:04 +0900

    std::thread編

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,51 +1,29 @@
 #include <iostream>
-#include <sstream>
-#include <vector>
-#include <memory>
-#include <algorithm>
-#include <regex>
+#include <thread>
 
-#include "Parser_.h"
 
 int main(void)
 {
-    std::unique_ptr<Parser> parser = std::make_unique<Parser>();
+    using namespace std;
 
-    parser->Load("text/PG3_2024_03_02.txt");
 
-    auto& parsedData = parser->GetArrays();
+    std::thread th1([]() {
+        cout << "thread 1" << endl;
+    });
+    th1.join();
 
-    std::sort(parsedData.back().begin(), parsedData.back().end());
 
-    /// 頑張って書いたやつ
-    //std::sort(parsedData.back().begin(), parsedData.back().end(), [](const std::string& _l, const std::string& _r) {
-    //    std::regex pattern("\\d+?");
-    //    std::smatch lMatch;
-    //    std::smatch rMatch;
+    std::thread th2([]() {
+        cout << "thread 2" << endl;
+    });
+    th2.join();
 
-    //    std::string ls = _l;
-    //    std::string rs = _r;
 
-    //    while (std::regex_search(ls, lMatch, pattern) && std::regex_search(rs, rMatch, pattern))
-    //    {
-    //        if (std::stoi(lMatch[0]) == std::stoi(rMatch[0]))
-    //        {
-    //            ls = lMatch.suffix();
-    //            rs = rMatch.suffix();
-    //            continue;
-    //        }
-    //        else return std::stoi(lMatch[0]) < std::stoi(rMatch[0]);
-    //    }
-    //    return false;
-    //});
+    std::thread th3([]() {
+        cout << "thread 3" << endl;
+    });
+    th3.join();
 
-    for (auto& array : parsedData)
-    {
-        for (auto& data : array)
-        {
-            std::cout << data << std::endl;
-        }
-    }
 
     return 0;
 }
